4a15ed9c52a4136338c201b006d59c26cff5ac80
akr <akr69905@gmail.com>
2026-01-08 00:25:39 +0530

    feat: Initial project setup with Spring Boot 3 and Dockerized PostgreSQL

---
+++ src/main/java/com/stratos/StratosApplication.java
@@ -0,0 +1,13 @@
+package com.stratos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StratosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StratosApplication.class, args);
+	}
+
+}
